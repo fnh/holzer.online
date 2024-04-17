ba5b9893696779a6075d6b77e669bd928f2cb55c
Fabian Holzer <fabian.holzer.lists+github@gmail.com>
2024-04-17 21:24:20 +0200

    Update sort-able

--- scripts/sort-able.js
+++ scripts/sort-able.js
@@ -1,16 +1,21 @@
 class SortTable extends HTMLTableElement {
 
-    static KEY_ENTER = 13;
-    static KEY_SPACE = 32;
+    static #KEY_ENTER = 13;
+    static #KEY_SPACE = 32;
 
-    static KEY_ARROW_UP = 38;
-    static KEY_ARROW_DOWN = 40;
+    static #KEY_ARROW_UP = 38;
+    static #KEY_ARROW_DOWN = 40;
 
-    static DIRECTION_OF_KEY = Object.entries({
-        "default": [SortTable.KEY_ENTER, SortTable.KEY_SPACE],
-        "asc": [SortTable.KEY_ARROW_UP],
-        "desc": [SortTable.KEY_ARROW_DOWN],
+    static #DIRECTION_OF_KEY = Object.entries({
+        "default": [SortTable.#KEY_ENTER, SortTable.#KEY_SPACE],
+        "asc": [SortTable.#KEY_ARROW_UP],
+        "desc": [SortTable.#KEY_ARROW_DOWN],
     });
+
+    static #COMPARATOR = {
+        asc: (a, b) => a.value.localeCompare(b.value),
+        desc: (a, b) => this.#COMPARATOR.asc(b, a)
+    }
 
     //---Custom Element Lifecycle Hooks----------------------------------------
 
@@ -19,12 +24,12 @@ class SortTable extends HTMLTableElement {
     }
 
     connectedCallback() {
-        this.setUp();
+        this.#setUp();
     }
 
     //---Generic utils---------------------------------------------------------
 
-    lookup({ inTable, valueOf }) {
+    #lookup({ inTable, valueOf }) {
         let tableEntry = inTable.find(([returnValue, candidates]) => candidates.includes(valueOf))
         if (tableEntry) {
             return tableEntry[0];
@@ -33,61 +38,42 @@ class SortTable extends HTMLTableElement {
 
     //---DOM utils-------------------------------------------------------------
 
-    hasHeaderRow() {
+    #hasHeaderRow() {
         return this.getElementsByTagName('thead').length !== 0
     }
 
-    getHeaderRow() {
+    #getHeaderRow() {
         return this.tHead?.rows[0];
     }
 
-    getTableBody() {
+    #getTableBody() {
         return this.tBodies[0];
     }
 
-    getHeaderCells() {
-        let tr = this.getHeaderRow();
+    #getHeaderCells() {
+        let tr = this.#getHeaderRow();
         return tr ? [...tr.cells] : [];
     }
 
 
     //---Setup-----------------------------------------------------------------
 
-    setUp() {
-        this.prepareHeader();
-    }
+    #setUp() {
+        this.#ensureHeaderRow();
 
-    prepareHeader() {
-        this.ensureHeaderRow();
-
-        this.getHeaderCells().forEach((th, index) => {
+        this.#getHeaderCells().forEach((th, index) => {
             th.tabIndex = 0;
             th.setAttribute("role", "button");
             th.style.cursor = "pointer";
 
-            this.on(th, "click", () => this.sortBy(index, "default"))
+            this.#on(th, "click", () => this.sortBy(index, "default"))
             const keydownHandler = (direction = "default") => this.sortBy(index, direction)
-            this.on(th, "keydown", (e) => this.onApplicableKeyDown(e, keydownHandler));
+            this.#on(th, "keydown", (e) => this.#onApplicableKeyDown(e, keydownHandler));
         });
     }
 
-    on(element, eventName, handler) {
-        element.addEventListener(eventName, handler, false);
-    }
-
-    onApplicableKeyDown(event, sortBy) {
-        const key = event.which;
-        const ordering =
-            this.lookup({ valueOf: key, inTable: SortTable.DIRECTION_OF_KEY });
-
-        if (ordering) {
-            event.preventDefault();
-            sortBy(ordering);
-        } 
-    }
-
-    ensureHeaderRow() {
-        if (!this.hasHeaderRow()) {
+    #ensureHeaderRow() {
+        if (!this.#hasHeaderRow()) {
             let headerElement = document.createElement('thead');
             let firstRow = this.rows[0];
 
@@ -96,64 +82,76 @@ class SortTable extends HTMLTableElement {
         }
     }
 
+    #on(element, eventName, handler) {
+        element.addEventListener(eventName, handler, false);
+    }
+
+    #onApplicableKeyDown(event, sortBy) {
+        const key = event.which;
+        const ordering =
+            this.#lookup({ valueOf: key, inTable: SortTable.#DIRECTION_OF_KEY });
+
+        if (ordering) {
+            event.preventDefault();
+            sortBy(ordering);
+        }
+    }
+
     //---Sorting---------------------------------------------------------------
 
     sortBy(column, direction = "asc") {
+        let orderedByDirection =
+            SortTable.#COMPARATOR[this.#getSortingOrder(direction, column)];
 
-        let tbody = this.getTableBody();
+        let tbody = this.#getTableBody();
         let rows = [...tbody.rows];
-
-        if (direction == "default") {
-            let currentOrder =
-                this.getHeaderCells()[column].dataset.sortAbleSortOrder;
-
-            if (currentOrder === "asc") {
-                direction = "desc";
-            }
-
-            if (currentOrder === "desc") {
-                direction = "asc";
-            }
-
-            if (!currentOrder) {
-                direction = "asc";
-            }
-        }
-
-        let comparator =
-            (a, b) => a.value.localeCompare(b.value);
-
-        let orderedBy =
-            direction === "desc"
-                ? (a, b) => comparator(b, a)
-                : comparator;
 
         let sortedRows = rows.map((row) => {
             return {
                 element: row,
-                value: this.getValue(row, column)
+                value: this.#getValue(row, column)
             }
-        }).sort(orderedBy);
+        }).sort(orderedByDirection);
 
         sortedRows.forEach(bag => {
             tbody.appendChild(bag.element);
         })
 
-        let headerCells = this.getHeaderCells();
-
-        headerCells.forEach((cell, index) => {            if (index == column) {
-                cell.dataset.sortAbleSortOrder = direction;
+        let headerCells = this.#getHeaderCells();
+        headerCells.forEach((cell, index) => {
+            if (index == column) {
+                cell.dataset.sortAbleSortOrder = this.#getSortingOrder(direction, column);
             } else {
                 delete cell.dataset.sortAbleSortOrder;
             }
         });
     }
 
-    getValue(row, column) {
+    #getSortingOrder(direction, column) {
+        if (direction == "default") {
+            let currentOrder =
+                this.#getHeaderCells()[column].dataset.sortAbleSortOrder;
+
+            if (currentOrder === "asc") {
+                return "desc";
+            }
+
+            if (currentOrder === "desc") {
+                return "asc";
+            }
+
+            if (!currentOrder) {
+                return "asc";
+            }
+        }
+
+        return direction;
+    }
+
+    #getValue(row, column) {
         return row.cells[column].innerText.trim();
     }
 
 }
-
 
 customElements.define("sort-able", SortTable, { extends: "table" });
